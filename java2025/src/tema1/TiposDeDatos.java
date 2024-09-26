22632912627946ebb4fcdd6188455012d2381b99
jjguillen <jjavierguillen@gmail.com>
2024-09-26 20:00:19 +0200

    ejemplo

---
+++ java2025/src/tema1/TiposDeDatos.java
@@ -0,0 +1,29 @@
+package tema1;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+
+        //Enteros -> int o long
+        int numero = 10000000;
+        int total = 100;
+
+        total = (total * numero);
+
+        System.out.println(total);
+
+        char caracter = 'a';
+
+        System.out.println(caracter);
+
+        caracter = 74;
+
+        System.out.println(caracter);  //Pinta representación ASCII del número 74 -> J
+
+
+
+
+
+    }
+
+}
