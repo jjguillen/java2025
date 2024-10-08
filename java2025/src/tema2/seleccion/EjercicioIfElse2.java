23d7b86ca97a0048e3f10661c7b93befbf9be5cc
jjguillen <jjavierguillen@gmail.com>
2024-10-08 18:59:23 +0200

    Ejemplos clase if-else

---
+++ java2025/src/tema2/seleccion/EjercicioIfElse2.java
@@ -0,0 +1,25 @@
+package tema2.seleccion;
+
+public class EjercicioIfElse2 {
+
+    public static void main(String[] args) {
+
+        // VidaJugador = 0-100
+        // VidaMonstruo = 0-50
+        // PuntosEspada = 0-50
+        // RunaMagica = true/false
+
+        // Es un combate entre el jugador y el monstruo.
+        // Gana el jugador si:
+        //          - VidaJugador > 50 y PuntosEspada > 30
+        //          - VidaJugador < 50 y PuntosEspada > 50
+        //          - VidaJugador < 50 y PuntosEspada < 30 y RunaMagica = true
+
+        // Gana el monstruo si:
+        //          - Si la vida del jugador es menor de 50 y los puntos
+        //          de la espada menor que 50,
+        //          O si la vida del jugador es menor de 50 y RunaMagica = false
+
+
+    }
+}
