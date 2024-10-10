8183ec5c8d0d07251d9e601d439b0adfadb5e607
jjguillen <jjavierguillen@gmail.com>
2024-10-10 20:48:19 +0200

    Ejercicios if

---
+++ java2025/src/tema2/seleccion/Ejercicio10Hoja.java
@@ -0,0 +1,35 @@
+package tema2.seleccion;
+
+import java.util.Scanner;
+
+public class Ejercicio10Hoja {
+    public static void main(String[] args) {
+        int a,b,c;
+        double solucion1, solucion2;
+
+        //ax2 + bx + c = 0
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime el valor de a");
+        a = Integer.parseInt(sc.nextLine());
+        System.out.println("Dime el valor de b");
+        b = Integer.parseInt(sc.nextLine());
+        System.out.println("Dime el valor de c");
+        c = Integer.parseInt(sc.nextLine());
+
+        int disc = b * b  - (4 * a * c);
+        if (a == 0) {
+            System.out.println("No hay solución, a es 0");
+        } else if (disc == 0) {
+            solucion1 = -1 * b / (2 * a);
+            System.out.println("Solución es: " + solucion1);
+        } else if (disc < 0) {
+            System.out.println("No tiene solución real");
+        } else if (disc > 0) {
+            solucion1 = -b + Math.sqrt(disc) / (2 * a);
+            solucion2 = -b - Math.sqrt(disc) / (2 * a);
+            System.out.println("La solución 1 es " + solucion1);
+            System.out.println("La solución 2 es " + solucion2);
+        }
+
+    }
+}
