7bdbd232422fcd2fda0d47102ede9cb59024c853
jjguillen <jjavierguillen@gmail.com>
2024-11-14 21:06:49 +0100

    Matrices

--- java2025/src/tema3/matrices/Ejercicio1.java
+++ java2025/src/tema3/matrices/Ejercicio1.java
@@ -21,7 +21,30 @@ public class Ejercicio1 {
         //Ej1 - Crea un matriz 6x6 de enteros. Rellénala de valores aleatorios y
         //      dime la suma de la diagonal principal y de la diagonal secundaria
 
+        int numeros[][] = new int[6][6];
 
+        int sumaDPr=0, sumaDSc=0;
+
+        for(int i=0; i < numeros.length; i++) {
+            for(int j=0; j < numeros[0].length; j++) {
+                numeros[i][j] = numero_aleatorio(1,9);
+            }
+        }
+
+        pintar(numeros);
+
+        for(int i=0; i < numeros.length; i++) {
+            for(int j=0; j < numeros[0].length; j++) {
+                if (i == j)
+                    sumaDPr += numeros[i][j];
+
+                if (i+j == numeros.length-1)
+                    sumaDSc += numeros[i][j];
+            }
+        }
+
+        System.out.println("Diagonal principal = " + sumaDPr);
+        System.out.println("Diagonal secundaria = " + sumaDSc);
 
 
 
