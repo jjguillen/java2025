fca7e93202d5912c1a7571e15f1337636df5d8c5
sjgui <jjavierguillen@gmail.com>
2024-12-15 23:02:03 +0100

    Prueba escrita 2024-2025

--- java2025/src/tema3/prueba2024_2025/Ejercicio3.java
+++ java2025/src/tema3/prueba2024_2025/Ejercicio3.java
@@ -2,80 +2,101 @@ package tema3.prueba2024_2025;
 
 public class Ejercicio3 {
 
-    public static String encriptar(String text) {
-        char aux;
-
-        StringBuilder resultado = new StringBuilder();
-        StringBuffer sb = new StringBuffer(text.toLowerCase());
-
-        //Intercambiar primera y última letra
-        aux = sb.charAt(text.length() - 1);
-        sb.setCharAt(sb.length() - 1, sb.charAt(0));
-        sb.setCharAt(0, aux);
-
-        //Reemplazar vocales
-        for(int i=0; i < sb.length(); i++) {
-            switch (sb.charAt(i)) {
-                case 'a': sb.setCharAt(i, '@'); break;
-                case 'e': sb.setCharAt(i, '&'); break;
-                case 'i': sb.setCharAt(i, '$'); break;
-                case 'o': sb.setCharAt(i, 'º'); break;
-                case 'u': sb.setCharAt(i, '#'); break;
+    public static void cryptVocales(StringBuffer sb, boolean encriptar) {
+        if (encriptar) {
+            for(int i=0; i < sb.length(); i++) {
+                switch (sb.charAt(i)) {
+                    case 'a': sb.setCharAt(i, '@'); break;
+                    case 'e': sb.setCharAt(i, '&'); break;
+                    case 'i': sb.setCharAt(i, '$'); break;
+                    case 'o': sb.setCharAt(i, 'º'); break;
+                    case 'u': sb.setCharAt(i, '#'); break;
+                }
+            }
+        } else {
+            for(int i=0; i < sb.length(); i++) {
+                switch (sb.charAt(i)) {
+                    case '@': sb.setCharAt(i, 'a'); break;
+                    case '&': sb.setCharAt(i, 'e'); break;
+                    case '$': sb.setCharAt(i, 'i'); break;
+                    case 'º': sb.setCharAt(i, 'o'); break;
+                    case '#': sb.setCharAt(i, 'u'); break;
+                }
             }
         }
+    }
 
-        //Invertir palabras de longitud impar
+    /**
+     * Pone la primera letra al final del StringBuffer
+     * @param sb
+     */
+    public static void primeraAlFinal(StringBuffer sb) {
+        String primeraLetra = sb.substring(0,1);
+        sb.deleteCharAt(0);
+        sb.append(primeraLetra);
+    }
+
+    /**
+     * Pone la última letra al principio
+     * @param sb
+     */
+    public static void ultimaAlPrincipio(StringBuffer sb) {
+        String ultimaLetra = sb.substring(sb.length()-1);
+        sb.deleteCharAt(sb.length()-1);
+        sb.insert(0,ultimaLetra);
+    }
+
+    /**
+     * Invertir las palabras impares
+     * @param sb
+     */
+    public static void invertirPalabras(StringBuffer sb) {
         String palabras[] = sb.toString().split(" ");
         for(int i=0; i < palabras.length; i++) {
             if (palabras[i].length() % 2 != 0) {
                 palabras[i] = new StringBuffer(palabras[i]).reverse().toString();            }
         }
 
+        //Limpiar StringBuffer
+        sb.setLength(0);
+
         //Unir las palabras
         for(int i=0; i < palabras.length; i++) {
-            resultado.append(palabras[i]).append(" ");
+            sb.append(palabras[i]).append(" ");
         }
 
-        return resultado.toString().trim();
+        //Eliminamos el último espacio
+        sb.deleteCharAt(sb.length()-1);
+    }
+
+    public static String encriptar(String text) {
+        StringBuffer sb = new StringBuffer(text.toLowerCase());
+
+        //Intercambiar primera y última letra
+        primeraAlFinal(sb);
+
+        //Reemplazar vocales
+        cryptVocales(sb, true);
+
+        //Invertir palabras de longitud impar
+        invertirPalabras(sb);
+
+        return sb.toString().trim();
     }
 
     public static String desencriptar(String text) {
-        char aux;
-
-        StringBuilder resultado = new StringBuilder();
         StringBuffer sb = new StringBuffer(text.toLowerCase());
 
         //Invertir palabras de longitud impar
-        String palabras[] = sb.toString().split(" ");
-        for(int i=0; i < palabras.length; i++) {
-            if (palabras[i].length() % 2 != 0) {
-                palabras[i] = new StringBuffer(palabras[i]).reverse().toString();            }
-        }
-
-        //Unir las palabras
-        for(int i=0; i < palabras.length; i++) {
-            resultado.append(palabras[i]).append(" ");
-        }
-        //Eliminamos el último espacio
-        resultado.deleteCharAt(resultado.length()-1);
+        invertirPalabras(sb);
 
         //Reemplazar vocales
-        for(int i=0; i < resultado.length(); i++) {
-            switch (resultado.charAt(i)) {
-                case '@': resultado.setCharAt(i, 'a'); break;
-                case '&': resultado.setCharAt(i, 'e'); break;
-                case '$': resultado.setCharAt(i, 'i'); break;
-                case 'º': resultado.setCharAt(i, 'o'); break;
-                case '#': resultado.setCharAt(i, 'u'); break;
-            }
-        }
+        cryptVocales(sb, false);
 
-        //Intercambiar primera y última letra
-        aux = resultado.charAt(resultado.length() - 1);
-        resultado.setCharAt(resultado.length() - 1, resultado.charAt(0));
-        resultado.setCharAt(0, aux);
+        //Última al principio
+        ultimaAlPrincipio(sb);
 
-        return resultado.toString();
+        return sb.toString();
     }
 
     public static void main(String[] args) {
