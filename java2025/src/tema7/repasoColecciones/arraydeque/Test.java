ed4c99da639a1b5c2621e2e54f9cdae87f814187
jjguillen <jjavierguillen@gmail.com>
2025-04-02 17:35:35 +0200

    Tema7 - Repaso colecciones

---
+++ java2025/src/tema7/repasoColecciones/arraydeque/Test.java
@@ -0,0 +1,12 @@
+package tema7.repasoColecciones.arraydeque;
+
+public class Test {
+    public static void main(String[] args) {
+
+        LogBuffer lg = new LogBuffer(10000);
+        for(int i=1; i <= 10005; i++)
+            lg.addLog("Mensaje " + i);
+
+        lg.showLogs();
+    }
+}
