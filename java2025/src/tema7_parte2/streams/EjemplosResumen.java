efd9f299d03d212b9ae7e007d844bc8010bc23e8
jjguillen <jjavierguillen@gmail.com>
2025-04-13 13:30:32 +0200

    List.of().stream() --> Stream.of()

--- java2025/src/tema7_parte2/streams/EjemplosResumen.java
+++ java2025/src/tema7_parte2/streams/EjemplosResumen.java
@@ -2,26 +2,24 @@ package tema7_parte2.streams;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class EjemplosResumen {
     public static void main(String[] args) {
 
-        long total = List.of(234,22,1,234,546,55,67676,77,58,2,900)
-                .stream()
+        long total = Stream.of(234,22,1,234,546,55,67676,77,58,2,900)
                 .count();
 
         System.out.println(total);
 
         //Mínimo
-        long min = List.of(234,22,1,234,546,55,67676,77,58,2,900)
-                .stream()
+        long min = Stream.of(234,22,1,234,546,55,67676,77,58,2,900)
                 .min(Comparator.naturalOrder())
                 .orElseThrow(); //Lanza excepción si no lo encuentra
 
         System.out.println(min);
 
-        long max = List.of(67676,234,22,1,234,546,55,67676,77,58,2,900)
-                .stream()
+        long max = Stream.of(67676,234,22,1,234,546,55,67676,77,58,2,900)
                 .max(Comparator.naturalOrder())
                 .orElseThrow(); //Lanza excepción si no lo encuentra
 
